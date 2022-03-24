4e472e07688c3c8146d8645f59b6070b599a36c4
gt <gurkanturkarslan@hotmail.com>
2022-03-24 13:28:00 +0300

    Add pre-population script

---
+++ src/main/resources/role.sql
@@ -0,0 +1,5 @@
+-- INSERT INTO ROLE(ROLE_TYPE) VALUES ( 'USER' ),
+--                                    ( 'INSTRUCTOR' ),
+--                                    ( 'MODERATOR' ),
+--                                    ( 'STUDENT' ),
+--                                    ( 'DEV' );
