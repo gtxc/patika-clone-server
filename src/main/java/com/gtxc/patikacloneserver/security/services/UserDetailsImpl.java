da5e44cb4b2ba876460616c5b7efac3f04190d8c
gt <gurkanturkarslan@hotmail.com>
2022-03-24 13:56:11 +0300

    Update security

--- src/main/java/com/gtxc/patikacloneserver/security/services/UserDetailsImpl.java
+++ src/main/java/com/gtxc/patikacloneserver/security/services/UserDetailsImpl.java
@@ -5,25 +5,26 @@ package com.gtxc.patikacloneserver.security.services;
     Project: patika-clone-server, Package: com.gtxc.patikacloneserver.service.
 */
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gtxc.patikacloneserver.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class UserDetailsImpl implements UserDetails {
 
     private static final long serialVersionUID = 1L;
-    private Long id;
-    private String username;
-    private String email;
+    private final Long id;
+    private final String username;
+    private final String email;
     @JsonIgnore
-    private String password;
-    private Collection<? extends GrantedAuthority> authorities;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
 
     public UserDetailsImpl(Long id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
         this.id = id;
