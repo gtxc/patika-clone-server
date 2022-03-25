4f8433179c5a4e185f576faeb81f159a1a630864
gt <gurkanturkarslan@hotmail.com>
2022-03-25 13:54:27 +0300

    Update RoleRepository.java

--- src/main/java/com/gtxc/patikacloneserver/repository/RoleRepository.java
+++ src/main/java/com/gtxc/patikacloneserver/repository/RoleRepository.java
@@ -10,6 +10,7 @@ import com.gtxc.patikacloneserver.model.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
